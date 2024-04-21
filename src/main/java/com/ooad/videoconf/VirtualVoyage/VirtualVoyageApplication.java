ed05e6fd1dcde02c08029dbcefe75d1e77fe2a9e
nigeldias27 <nigeldias27@yahoo.com>
2024-04-21 15:34:48 +0530

    Initial commit

---
+++ src/main/java/com/ooad/videoconf/VirtualVoyage/VirtualVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.ooad.videoconf.VirtualVoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualVoyageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooad/videoconf/VirtualVoyage/VirtualVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooad.videoconf.VirtualVoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
